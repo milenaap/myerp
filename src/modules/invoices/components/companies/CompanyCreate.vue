4d625699e01338a1bcac7a70c65da778a71368c2
Dorian Gonzalez <doriandarren1@gmail.com>
2024-06-20 10:08:12 +0200

    add combo list

--- src/modules/invoices/components/companies/CompanyCreate.vue
+++ src/modules/invoices/components/companies/CompanyCreate.vue
@@ -10,9 +10,7 @@
 
 				<div class="col-span-12 md:col-span-6 lg:col-span-4">
 					<div class="input-form">
-						<label for="country_id" class="form-label w-full">
-							{{ $t("country_id") }} *
-						</label>
+						
 						<!-- <input
 							v-model.trim="validate.country_id.$model"
 							id="country_id"
@@ -22,6 +20,9 @@
 							:class="{ 'border-danger': validate.country_id.$error }"
 						/> -->
 
+						<!-- <label for="country_id" class="form-label w-full">
+							{{ $t("country_id") }} *
+						</label>
 						<select 
 							v-model.trim="validate.country_id.$model"
 							id="country_id"
@@ -39,7 +40,36 @@
 								{{ item.common_name }}
 							</option>
 
-						</select>
+						</select> -->
+
+
+
+						<Combobox as="div" v-model="selectedPerson" @update:modelValue="query = ''">
+							<ComboboxLabel class="form-label w-full">{{ $t("country_id") }} *</ComboboxLabel>
+							<div class="relative mt-0">
+							<ComboboxInput class="w-full rounded-md border-0 bg-white py-2 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" @change="query = $event.target.value" @blur="query = ''" :display-value="(person) => person?.name" />
+							<ComboboxButton class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+								<ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+							</ComboboxButton>
+
+							<ComboboxOptions v-if="filteredPeople.length > 0" class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+								<ComboboxOption v-for="person in filteredPeople" :key="person.id" :value="person" as="template" v-slot="{ active, selected }">
+								<li :class="['relative cursor-default select-none py-2 pl-8 pr-4', active ? 'bg-indigo-600 text-white' : 'text-gray-900']">
+									<span :class="['block truncate', selected && 'font-semibold']">
+									{{ person.name }}
+									</span>
+
+									<span v-if="selected" :class="['absolute inset-y-0 left-0 flex items-center pl-1.5', active ? 'text-white' : 'text-indigo-600']">
+									<CheckIcon class="h-5 w-5" aria-hidden="true" />
+									</span>
+								</li>
+								</ComboboxOption>
+							</ComboboxOptions>
+							</div>
+						</Combobox>
+
+
+
 
 						<template v-if="validate.country_id.$error">
 							<div v-for="(error, index) in validate.country_id.$errors" :key="index" class="text-danger mt-2">
@@ -275,12 +305,44 @@
 
 <script setup>
 
-	import { onMounted, reactive, toRefs } from 'vue';
+	import { onMounted, reactive, toRefs, ref, computed } from 'vue';
 	import { required, minLength, maxLength, email, url, integer } from '@vuelidate/validators';
 	import { useVuelidate } from '@vuelidate/core';
 	import { helpers } from '@vuelidate/validators';
 	import { useI18n } from 'vue-i18n';
 	import useCountry from "../../composables/countries";
+
+
+
+
+	import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+	import {
+	Combobox,
+	ComboboxButton,
+	ComboboxInput,
+	ComboboxLabel,
+	ComboboxOption,
+	ComboboxOptions,
+	} from '@headlessui/vue'
+
+
+	const people = [
+  { id: 1, name: 'Leslie Alexander' },
+	// More users...
+	]
+
+	const query = ref('')
+	const selectedPerson = ref(null)
+	const filteredPeople = computed(() =>
+	query.value === ''
+		? people
+		: people.filter((person) => {
+			return person.name.toLowerCase().includes(query.value.toLowerCase())
+		}),
+	)
+
+
+
 
 
 	const {countries, getCountries} = useCountry();
